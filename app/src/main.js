6fab5bd1bb3b65b21d147234b78872b51c52f672
Jaime Tarrant <jdt@cookiesystems.com>
2014-05-19 00:24:32 +1000

    Updated styling

--- app/src/main.js
+++ app/src/main.js
@@ -7,67 +7,122 @@ define(function(require, exports, module) {
   var InputSurface = require('famous/surfaces/InputSurface');
   var ImageSurface = require('famous/surfaces/ImageSurface');
   var Modifier = require('famous/core/Modifier');
-  var SequentialLayout = require("famous/views/SequentialLayout");
+  var Transform = require('famous/core/Transform');
+  var StateModifier = require('famous/modifiers/StateModifier');
+  var SequentialLayout = require('famous/views/SequentialLayout');
 
-  // create the main context
+  var StateModifier = require('famous/modifiers/StateModifier');
+  var Transitionable = require('famous/transitions/Transitionable');
+  var SpringTransition = require('famous/transitions/SpringTransition');
+  Transitionable.registerMethod('spring', SpringTransition);
+
   var mainContext = Engine.createContext();
 
-  var sequentialLayout = new SequentialLayout({
-    direction: 1,
-  });
-  var surfaces = [];
-
-  surfaces.push(new ImageSurface({ 
-    // your app here
-
-    //  var surface = new Surface({
-    classes: [],
-    size: [250,250],
-    content: 'content/images/famous_symbol_transparent.png'}));
-
-  surfaces.push(new InputSurface({
-    size: [250, 50],
-    classes: ['loginInput'],
-    name: 'email',
-    id: 'email',
-    placeholder: 'Email address',
-    value: '',
-    type: 'text'
-  }));
-
-  surfaces.push(new InputSurface({
-    size: [250, 50],
-    classes: ['loginInput'],
-    id: 'password',
-    name: 'password',
-    placeholder: 'Password',
-    value: '',
-    type: 'password'
-  }));
-
-  var outlineModifier = new Modifier({
-    origin: [0.5, 0.2]
+  var loginButtonModifier = new StateModifier({
+    origin: [0.5, 0]
   });
 
-  var inputModifier = new Modifier({
-    origin: [0.5, 0.5]
-  });
+  createLogo();
+  createLoginEmail();
+  createLoginPassword();
+  createLoginButton();
 
-  sequentialLayout.sequenceFrom(surfaces);
+  function createLogo() {
+    var logoSurface = new ImageSurface({
+      classes: [],
+      size: [250,250],
+      content: 'content/images/famous_symbol_transparent.png'
+    });
+
+    /*
+       var stateModifier = new StateModifier({
+transform: Transform.translate(150, 100, 0)
+});
+*/
+    var logoPositionModifier = new Modifier({
+      origin: [window.innerHeight / (window.innerHeight * 2), window.innerHeight / (window.innerHeight * 2) - 0.2] 
+    });
+
+    mainContext.add(logoPositionModifier).add(logoSurface);
+  }
+
+  function createLoginEmail() {
+    var loginEmailSurface = new InputSurface({
+      size: [250, 50],
+      color: 'grey',
+      classes: ['loginInput'],
+      name: 'email',
+      id: 'email',
+      placeholder: 'Email address',
+      value: '',
+      type: 'text'
+    });
+    var loginEmailSurfaceModifier = new Modifier({
+      origin: [window.innerHeight / (window.innerHeight * 2), window.innerHeight / (window.innerHeight * 2) + 0.4] 
+    });
+
+    mainContext.add(loginEmailSurfaceModifier).add(loginEmailSurface);
+  }
+
+  function createLoginPassword() {
+    var loginPasswordSurface = new InputSurface({
+      size: [250, 50],
+      classes: ['loginInput'],
+      id: 'password',
+      name: 'password',
+      placeholder: 'Password',
+      value: '',
+      type: 'password'
+    });
+
+    var loginPasswordSurfaceModifier = new Modifier({
+      origin: [window.innerHeight / (window.innerHeight * 2), window.innerHeight / (window.innerHeight * 2) + 0.6] 
+    });
+
+    mainContext.add(loginPasswordSurfaceModifier).add(loginPasswordSurface);
+  }
+
+
+  function createLoginButton() {
+    var loginButtonSurface = new Surface({
+      size: [200, 60],
+      content: '<br /> <b>Login</b>',
+      properties: {
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: 'green'
+      }
+    });
+
+    var spring = {
+      method: 'spring',
+      period: 1000,
+      dampingRatio: 0.4
+    };
+
+    loginButtonModifier.setTransform(
+      Transform.translate(0, 465, 0), spring
+    );
+
+    mainContext.add(loginButtonModifier).add(loginButtonSurface);
+
+  }
+
+  //sequentialLayout.sequenceFrom(surfaces);
 /*
 
-  for (var i = 0; i < 10; i++) {
-    surfaces.push(new Surface({
-      content: "Surface: " + (i + 1),
-      size: [undefined, window.innerHeight/10],
-      properties: {
-        backgroundColor: "hsl(" + (i * 360 / 10) + ", 100%, 50%)",
-        lineHeight: window.innerHeight/10 + "px",
-        textAlign: "center"
-      }
-    }));
-  }
+   for (var i = 0; i < 10; i++) {
+   surfaces.push(new Surface({
+content: "Surface: " + (i + 1),
+size: [undefined, window.innerHeight/10],
+properties: {
+backgroundColor: "hsl(" + (i * 360 / 10) + ", 100%, 50%)",
+lineHeight: window.innerHeight/10 + "px",
+textAlign: "center"
+}
+}));
+}
 */
-  mainContext.add(outlineModifier).add(sequentialLayout);
+
   //  mainContext.add(inputModifier).add(surfaces);
 });
